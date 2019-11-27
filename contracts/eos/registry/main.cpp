b76eb21355d9432a6628b190b0aabdacfdfc9d08
shakhruz@gmail.com <shakhruz@gmail.com>
2019-11-27 22:43:24 +0500

    new registry contract with vram and vaccount

--- contracts/eos/registry/main.cpp
+++ contracts/eos/registry/main.cpp
@@ -1,21 +1,30 @@
+#define VACCOUNTS_DELAYED_CLEANUP 120
+
 #include "../dappservices/multi_index.hpp"
+#include "../dappservices/ipfs.hpp"
+#include "../dappservices/vaccounts.hpp"
+
 #define DAPPSERVICES_ACTIONS() \
     XSIGNAL_DAPPSERVICE_ACTION \
-    IPFS_DAPPSERVICE_ACTIONS
+    IPFS_DAPPSERVICE_ACTIONS   \
+    VACCOUNTS_DAPPSERVICE_ACTIONS
+
 #define DAPPSERVICE_ACTIONS_COMMANDS() \
-    IPFS_SVC_COMMANDS()
+    IPFS_SVC_COMMANDS()                \
+    VACCOUNTS_SVC_COMMANDS()
+
 #define CONTRACT_NAME() registry
 using std::string;
+
 CONTRACT_START()
 
 TABLE item
 {
-    name item_name;
+    uint64_t prim_key;
+    name vaccount;
     std::vector<char> content;
-    bool is_alias;
-    name schema_scope;
-    name schema_name;
-    uint64_t primary_key() const { return item_name.value; }
+    checksum256 file_hash;
+    uint64_t primary_key() const { return prim_key; }
 };
 
 typedef dapp::multi_index<"vitems"_n, item> cold_items_t;
@@ -28,36 +37,33 @@ TABLE shardbucket
 };
 typedef eosio::multi_index<"vitems"_n, shardbucket> cold_items_t_abi;
 
-[[eosio::action]] void regitem(name owner, item new_item) {
-    require_auth(owner);
+ACTION regitem(item new_item)
+{
+    // require_auth(owner);
+    require_vaccount(new_item.vaccount);
 
-    cold_items_t items(_self, owner.value);
-    auto existing = items.find(new_item.item_name.value);
-    if (existing == items.end())
-    {
-        items.emplace(_self, [&](auto &a) {
-            a.item_name = new_item.item_name;
-            a.content = new_item.content;
-            a.is_alias = new_item.is_alias;
-            a.schema_scope = new_item.schema_scope;
-            a.schema_name = new_item.schema_name;
-        });
-    }
-    else
-    {
-        items.modify(*existing, eosio::same_payer, [&](auto &a) {
-            a.item_name = new_item.item_name;
-            a.content = new_item.content;
-            a.is_alias = new_item.is_alias;
-            a.schema_scope = new_item.schema_scope;
-            a.schema_name = new_item.schema_name;
-        });
-    }
+    cold_items_t items(_self, _self.value);
+    items.emplace(_self, [&](auto &a) {
+        a.prim_key = items.available_primary_key();
+        a.vaccount = new_item.vaccount;
+        a.content = new_item.content;
+        a.file_hash = new_item.file_hash;
+    });
 };
 
-[[eosio::action]] void warmupitem(name owner, item new_item) {
-    cold_items_t items(_self, owner.value);
-    auto existing = items.find(new_item.item_name.value);
+ACTION warmupitem(name owner, uint64_t prim_key)
+{
+    cold_items_t items(_self, prim_key);
+    auto existing = items.find(prim_key);
 }
 
-CONTRACT_END((regitem)(warmupitem))
+// ACTION clear() {
+//     require_auth(_self);
+
+//     cold_items_t items(_self, _self);
+// };
+
+VACCOUNTS_APPLY(((item)(regitem)))
+
+CONTRACT_END((regitem)(warmupitem)(xdcommit)(regaccount)(xvinit));
+// CONTRACT_END((regitem)(warmupitem))
